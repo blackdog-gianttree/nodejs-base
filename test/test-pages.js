3475f282a3674fb396404674bbebe76f812ba6bb
blackdog-gianttree <blackdog.gianttree@gmail.com>
2018-11-18 22:50:37 -0800

    hello world

---
+++ test/test-pages.js
@@ -0,0 +1,11 @@
+/* global it */
+
+const { expect } = require('chai');
+const request = require('request');
+
+it('Main page content', (done) => {
+  request('http://localhost:3000', (error, response, body) => {
+    expect(body).to.equal('Hello World\n');
+    done();
+  });
+});
